76e5ae3c50cc3513ca5dd94ed5947c92a05338da
Ismael Ishy Rivera <coralblue79@gmail.com>
2016-11-21 18:53:59 -0200

    lslider - time/6hr:53min

--- fcc-porta/js.js
+++ fcc-porta/js.js
@@ -1,24 +1,24 @@
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+// var myCenter = new google.maps.LatLng(41.878114, -87.629798);
 
-function initialize() {
-var mapProp = {
-center:myCenter,
-zoom:12,
-scrollwheel:false,
-draggable:false,
-mapTypeId:google.maps.MapTypeId.ROADMAP
-};
+// function initialize() {
+// var mapProp = {
+// center:myCenter,
+// zoom:12,
+// scrollwheel:false,
+// draggable:false,
+// mapTypeId:google.maps.MapTypeId.ROADMAP
+// };
 
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-var marker = new google.maps.Marker({
-position:myCenter,
-});
+// var marker = new google.maps.Marker({
+// position:myCenter,
+// });
 
-marker.setMap(map);
-}
+// marker.setMap(map);
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -52,3 +52,10 @@ $(document).ready(function(){
     } // End if
   });
 })
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ $(document).ready(function() {
+    $("#lightSlider").lightSlider(); 
+  });
